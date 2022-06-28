1829f0b8bb082025bca0c38f090a39a59544cde2
Thomas Weiler <tw@interactive-pioneers.de>
2022-06-28 15:16:44 +0200

    refactor: Move test files, ignore in NPM package

--- index.test.js
+++ index.test.js
@@ -13,32 +13,32 @@ async function run (input, output, opts = { }) {
 
 it('gets critical', async () => {
   await run(
-    fs.readFileSync('./testsource.css', {encoding:'utf8', flag:'r'}),
-    fs.readFileSync('./testresult_critical.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testsource.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testresult_critical.css', {encoding:'utf8', flag:'r'}),
     {parts: ['critical']}
   );
 });
 
 it('gets critical and essential', async () => {
   await run(
-    fs.readFileSync('./testsource.css', {encoding:'utf8', flag:'r'}),
-    fs.readFileSync('./testresult_critical_essential.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testsource.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testresult_critical_essential.css', {encoding:'utf8', flag:'r'}),
     {parts: ['essential','critical']}
   );
 });
 
 it('gets critical and essential with string config', async () => {
   await run(
-    fs.readFileSync('./testsource.css', {encoding:'utf8', flag:'r'}),
-    fs.readFileSync('./testresult_critical_essential.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testsource.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testresult_critical_essential.css', {encoding:'utf8', flag:'r'}),
     {parts: 'essential critical'}
   );
 });
 
 it('gets non-critical', async () => {
   await run(
-    fs.readFileSync('./testsource.css', {encoding:'utf8', flag:'r'}),
-    fs.readFileSync('./testresult_noncritical.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testsource.css', {encoding:'utf8', flag:'r'}),
+    fs.readFileSync('./test/testresult_noncritical.css', {encoding:'utf8', flag:'r'}),
     {parts: ['noncritical']}
   );
 });
